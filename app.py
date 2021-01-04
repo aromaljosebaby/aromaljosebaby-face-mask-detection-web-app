e9100177ba3821a5f8d2a830dbccb842b8de11c8
aromaljosebaby <65164420+aromaljosebaby@users.noreply.github.com>
2021-01-04 12:52:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,95 +1,117 @@
 import streamlit as st
-
-import cv2
-#from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import streamlit as st
+import cv2 as cv
+import tempfile
+import classify
 import numpy as np
-#import tensorflow as tf
-#import classify
 
-def main():
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+def load_haarcascade():
+    haarcascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    return haarcascade
 
-    html_temp = """
+haarcascade = load_haarcascade()
+
+
+rect_size = 4
+prediction_dictionary = {0: 'with_mask', 1: 'without_mask'}
+colour_dict = {0: (0, 255, 0), 1: (0, 0, 255)}
+
+
+
+html_temp = """
         <div style="background-color:#e63946 ;padding:10px">
         <h2 style="color:white;text-align:center;">Deep Learning Web App</h2>
         </div><br><br>
         """
 
-    st.markdown(html_temp, unsafe_allow_html=True)
+st.markdown(html_temp, unsafe_allow_html=True)
 
-    st.title("Mask Detection")
+st.title("Mask Detection")
 
-    st.title(" ")
-
-
-
-    st.title(" ")
-    st.title(" ")
-
-    variable=0
-
-
-    '''def load_haarcascade():
-        haarcascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        return haarcascade
-
-    haarcascade=load_haarcascade()'''
+st.title(" ")
 
 
 
+st.title(" ")
+st.title(" ")
 
-    rect_size = 4
-    prediction_dictionary={0: 'with_mask', 1: 'without_mask'}
-    colour_dict={0:(0,255,0),1:(0,0,255)}
 
-    button=st.button('Start Video')
+start_button = st.button('Start Video')
 
-    #if button:
 
-    cap = cv2.VideoCapture(0)
+
+#f = st.file_uploader("Upload file")
+
+if start_button:
+
+    #tfile = tempfile.NamedTemporaryFile(delete=False)
+    #tfile.write(f.read())
+
+    print('In')
+    #vf = cv.VideoCapture(tfile.name)
+
+    stframe = st.empty()
+    end_button = st.button('End Video')
+
+    cap = cv.VideoCapture(0)
     while True:
-        (rval, im) = cap.read()
-        im = cv2.flip(im, 1, 1)
+
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        #stframe.image(gray)
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        #frame=cv.rotate(frame,cv.cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 
 
-        rerect_size = cv2.resize(im, (im.shape[1] // rect_size, im.shape[0] // rect_size))
-        #faces = haarcascade.detectMultiScale(rerect_size)
-        '''for f in faces:
+
+
+
+
+
+
+        rerect_size = cv.resize(frame, (frame.shape[1] // rect_size, frame.shape[0] // rect_size))
+        faces = haarcascade.detectMultiScale(rerect_size)
+        for f in faces:
             (x, y, w, h) = [v * rect_size for v in f]
 
-            face_img = im[y: y + h, x: x + w]
+            face_img = frame[y: y + h, x: x + w]
 
-            img = cv2.resize(im, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+            img = cv.resize(frame, dsize=(224, 224), interpolation=cv.INTER_CUBIC)
 
-            #img = img_to_array(img)
+            img = img_to_array(img)
 
             img = img / 255.0
 
             lol = np.array([img])
 
-            #label,final_prediction=classify.predict(lol)
+            label,final_prediction=classify.predict(lol)
 
             #ans = loaded_model(lol, training=False)
 
 
 
-            #cv2.rectangle(im, (x, y), (x + w, y + h), colour_dict[label], 2)
-            #cv2.rectangle(im, (x, y - 40), (x + w, y), colour_dict[label], -1)
-            #cv2.putText(im, final_prediction, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)'''
+            cv.rectangle(frame, (x, y), (x + w, y + h), colour_dict[label], 2)
+            cv.rectangle(frame, (x, y - 40), (x + w, y), colour_dict[label], -1)
+            cv.putText(frame, final_prediction, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
 
-        cv2.imshow('LIVE', im)
-        key = cv2.waitKey(10)
+        #cv.imshow('LIVE', frame)
+        stframe.image(frame)
 
-        if key == 27:
+
+        if end_button:
+            stframe = st.empty()
+            cap.release()
             break
 
-    cap.release()
-
-    cv2.destroyAllWindows()
-
-    st.title(" ")
-    st.subheader('Press Escape key to close Video')
 
 
-if __name__=='__main__':
-    main()
+    #cv.destroyAllWindows()
+    print('done')
+
+
